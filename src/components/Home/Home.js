41eeae5e3d1cab762a8088f2e779caa5ca7395e8
moiz0304 <amoiz.bscs21seecs@seecs.edu.pk>
2024-05-19 14:28:25 +0500

    adding files

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to AucSol</h1>
+      <p>Your online auction solution.</p>
+    </div>
+  );
+};
+
+export default Home;
