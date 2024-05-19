33a1c521f7ad5251f1c4d984c049b66370d29da5
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-05-19 13:37:25 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import AuctionList from './components/AuctionList';
+import AuctionItem from './components/AuctionItem';
+import CreateAuction from './components/CreateAuction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/auctions" component={AuctionList} />
+          <Route path="/auction/:id" component={AuctionItem} />
+          <Route path="/create-auction" component={CreateAuction} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
